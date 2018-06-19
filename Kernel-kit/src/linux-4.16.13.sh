e337da252e4461efc1836d370a9afdccb21cde4a
Kálmán Staudinger <39849404+Skamilinux@users.noreply.github.com>
2018-06-19 10:41:14 +0000

    Update linux-4.16.13.sh

--- Kernel-kit/src/linux-4.16.13.sh
+++ Kernel-kit/src/linux-4.16.13.sh
@@ -1,5 +1,5 @@
 #download the kernel
-wget -c -N --progress=bar:force --content-disposition --trust-server-names -P $(PWD)/work https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.16.13.tar.xz
+wget -c -N --progress=bar:force --content-disposition --trust-server-names https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.16.13.tar.xz
 
 #unpack the kernel
 tar -xvf linux-4.16.13.tar.xz -C ../work/kernel
