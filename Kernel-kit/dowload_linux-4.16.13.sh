c1904f7d869e04952e50e947785ed8c878c8cdbe
Kálmán Staudinger <39849404+Skamilinux@users.noreply.github.com>
2018-06-20 05:31:44 +0000

    Update and rename linux-4.16.13.sh to dowload_linux-4.16.13.sh

--- Kernel-kit/dowload_linux-4.16.13.sh
+++ Kernel-kit/dowload_linux-4.16.13.sh
@@ -2,4 +2,4 @@
 wget -c -N --progress=bar:force --content-disposition --trust-server-names https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.16.13.tar.xz
 
 #unpack the kernel
-tar -xvf linux-4.16.13.tar.xz -C ../work/kernel
+tar -xvf linux-4.16.13.tar.xz -C linux-4.16.13
