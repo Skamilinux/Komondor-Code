3644fdc59982877b4a6758db38272978c661eb3b
Kálmán Staudinger <39849404+Skamilinux@users.noreply.github.com>
2018-06-20 06:00:20 +0000

    Create dowload_linux-4.16.13.sh

---
+++ Kernel-kit/linux-4.16.13-noPAE/dowload_linux-4.16.13.sh
@@ -0,0 +1,5 @@
+#download the kernel
+wget -c -N --progress=bar:force --content-disposition --trust-server-names https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.16.13.tar.xz
+
+#unpack the kernel
+tar -xvf linux-4.16.13.tar.xz -C kernel
